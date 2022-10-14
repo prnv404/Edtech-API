da2cf641c731202d07cdae05a0afe75a21510982
pranav <pranavofficial404@gmail.com>
2022-10-14 20:50:03 +0530

    typos

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,4 +1,3 @@
-const asyncHandler = require('express-async-handler');
 const CustomError = require('../errors');
 const User = require('../models/user-model');
 const Verify = require('../models/verification-model');
@@ -7,6 +6,11 @@ const { StatusCodes } = require('http-status-codes');
 const { attachCookieToResponse, createTokenUser } = require('../utils');
 const { createOTP, verifyOTP } = require('../utils/create-otp');
 
+/**
+ * It creates a new user in the database
+ * @param req - The request object.
+ * @param res - The response object.
+ */
 const signup = async (req, res) => {
    const { name, phone, password, standred } = req.body;
 
@@ -27,6 +31,12 @@ const signup = async (req, res) => {
    res.status(StatusCodes.OK).json({ message: 'done' });
 };
 
+/**
+ * It takes in a phone number and password, checks if the user exists, compares the password, and sends
+ * an OTP to the user
+ * @param req - The request object.
+ * @param res - The response object.
+ */
 const login = async (req, res) => {
    const { password, phone } = req.body;
 
@@ -45,11 +55,17 @@ const login = async (req, res) => {
    res.status(StatusCodes.OK).json({ message: 'done' });
 };
 
+/**
+ * It checks if the OTP is valid, if it is, it creates a verified document, creates a tokenUser,
+ * attaches the tokenUser to the response and returns the tokenUser
+ * @param req - The request object.
+ * @param res - The response object
+ */
 const check = async (req, res) => {
    const { phone, OTP } = req.body;
 
    const isVerified = await verifyOTP({ phoneNumber: phone, code: OTP });
-   
+
    if (isVerified.valid === false) {
       throw new CustomError.BadRequestError('Incorrect OTP');
    }
@@ -59,6 +75,11 @@ const check = async (req, res) => {
    attachCookieToResponse({ res, user: tokenUser, verified });
    res.status(StatusCodes.OK).json({ tokenUser });
 };
+/**
+ * It sets the cookie token to logout and sets the expiration date to the current date
+ * @param req - The request object.
+ * @param res - The response object.
+ */
 
 const logout = async (req, res) => {
    res.cookie('token', 'logout', {
