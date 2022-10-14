27a5cbc8ebb096697dae5dd4fcf8655cce0b0490
pranav <pranavofficial404@gmail.com>
2022-10-14 07:33:36 +0530

    eslint created

--- controllers/course-controller.js
+++ controllers/course-controller.js
@@ -1,63 +1,53 @@
 import asynchandler from 'express-async-handler'
-import Course from '../models/course-model.js'
 import { StatusCodes } from 'http-status-codes'
+import Course from '../models/course-model.js'
 import CustomError from '../errors/index.js'
 
 const getSubjects = asynchandler(async (req, res) => {
-  const standerd = req.user.standerd
-  const subject = await Course.find({ standerd: standerd }).select('subject')
-  console.log(subject)
+   const { standerd } = req.user
+   const subject = await Course.find({ standerd }).select('subject')
+   //  console.log(subject)
 
-  res.status(StatusCodes.OK).json(subject)
+   res.status(StatusCodes.OK).json(subject)
 })
 
 const getchapters = asynchandler(async (req, res) => {
-  const sub = req.query.subject
+   const sub = req.query.subject
 
-  const chapters = await Course.find({ subject: sub }).select(
-    'chapterNo chapterName'
-  )
+   const chapters = await Course.find({ subject: sub }).select('chapterNo chapterName')
 
-  res.status(StatusCodes.OK).json(chapters)
+   res.status(StatusCodes.OK).json(chapters)
 })
 
 const getAllVideo = asynchandler(async (req, res) => {
-  const { chapterNo, subject } = req.query
-  const videoId = req.params.id
-  console.log(videoId)
-  const video = await Course.findOne({
-    _id: videoId,
-  })
+   const videoId = req.params.id
+   //  console.log(videoId)
+   const video = await Course.findOne({
+      _id: videoId,
+   })
 
-  res.status(StatusCodes.OK).json(video)
+   res.status(StatusCodes.OK).json(video)
 })
 
 const getSingleVideo = asynchandler(async (req, res) => {
-  const { chapterNo, subject } = req.query
-  const videoId = req.params.id
-  console.log(videoId)
-  const video = await Course.findOne({
-    _id: videoId,
-  })
+   //  const { chapterNo, subject } = req.query
+   const videoId = req.params.id
+   //  console.log(videoId)
+   const video = await Course.findOne({
+      _id: videoId,
+   })
 
-  res.status(StatusCodes.OK).json(video)
+   res.status(StatusCodes.OK).json(video)
 })
 
 const createCourse = asynchandler(async (req, res) => {
-  const course = await Course.create(req.body)
-  res.send(course)
+   const course = await Course.create(req.body)
+   res.send(course)
 })
 
 const vidoeUpload = asynchandler(async (req, res, next) => {
-  console.log('video upload')
-  next()
+   //  console.log('video upload')
+   next()
 })
 
-export {
-  getchapters,
-  getAllVideo,
-  createCourse,
-  getSingleVideo,
-  getSubjects,
-  vidoeUpload,
-}
+export { getchapters, getAllVideo, createCourse, getSingleVideo, getSubjects, vidoeUpload }
