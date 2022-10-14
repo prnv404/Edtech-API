17acfd1c9b395af6bfbdcede3998f4a5053bd7a8
pranav <pranavofficial404@gmail.com>
2022-10-14 16:08:30 +0530

    error handled

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -6,67 +6,69 @@ const { attachCookieToResponse, createTokenUser } = require('../utils');
 const { createOTP, verifyOTP } = require('../utils/create-otp');
 
 const signup = asyncHandler(async (req, res) => {
-  const { name, phoneNumber, password, standerd } = req.body;
+    const { name, phoneNumber, password, standerd } = req.body;
 
-  if (!name || !phoneNumber || !password || !standerd) {
-    throw new CustomError.BadRequestError('please provide all values');
-  }
-  const isFirstAccount = (await User.countDocuments({})) === 0;
+    if (!name || !phoneNumber || !password || !standerd) {
+        throw new CustomError.BadRequestError('please provide all values');
+    }
+    const isFirstAccount = (await User.countDocuments({})) === 0;
 
-  const role = isFirstAccount ? 'admin' : 'user';
-  const user = await User.create({
-    name,
-    phoneNumber,
-    password,
-    standerd,
-    role,
-  });
+    const role = isFirstAccount ? 'admin' : 'user';
+    const user = await User.create({
+        name,
+        phoneNumber,
+        password,
+        standerd,
+        role,
+    });
+    createOTP({ res, phonenumber: phoneNumber, channel: 'sms' })
+    const tokenUser = createTokenUser(user);
+    attachCookieToResponse({ res, user: tokenUser });
 
-  const tokenUser = createTokenUser(user);
-  attachCookieToResponse({ res, user: tokenUser });
-
-  res.status(StatusCodes.CREATED).json({ tokenUser });
+    res.status(StatusCodes.CREATED).json({ tokenUser });
 });
 
 const login = asyncHandler(async (req, res) => {
-  const { phoneNumber, password } = req.body;
+    const { phoneNumber, password } = req.body;
 
-  if (!phoneNumber || !password) {
-    throw new CustomError.BadRequestError('Please provide all values');
-  }
-  const user = await User.findOne({ phoneNumber });
-  if (!user) {
-    throw new CustomError.NotFound(`No user found with this ${phoneNumber}`);
-  }
+    if (!phoneNumber || !password) {
+        throw new CustomError.BadRequestError('Please provide all values');
+    }
+    const user = await User.findOne({ phoneNumber });
+    if (!user) {
+        throw new CustomError.NotFound(
+            `No user found with this ${phoneNumber}`
+        );
+    }
 
-  const isMatch = await user.comparePassword(password);
+    const isMatch = await user.comparePassword(password);
 
-  if (!isMatch) {
-    throw new CustomError.UnAuthorized('Incorrect Password');
-  }
+    if (!isMatch) {
+        throw new CustomError.UnAuthorized('Incorrect Password');
+    }
 
-  createOTP({ res, phonenumber: phoneNumber, channel: 'sms' });
-  // const tokenUser = createTokenUser(user);
-  // attachCookieToResponse({ res, user: tokenUser });
+    createOTP({ res, phonenumber: phoneNumber, channel: 'sms' });
+    // const tokenUser = createTokenUser(user);
+    // attachCookieToResponse({ res, user: tokenUser });
 
-  res.status(StatusCodes.OK);
+    res.status(StatusCodes.OK);
 });
 
 const otp = asyncHandler(async (req, res) => {
-  const { mobileNumber, otp } = req.query;
- verifyOTP({ res, mobileNO: mobileNumber, code: otp });
+    const { mobileNumber, otp } = req.query;
+    verifyOTP({ res, mobileNO: mobileNumber, code: otp });
 });
 
 /* This is a function that is called when a user logs out. It takes in the request and response
 objects. It then sets the cookie to logout. It then sends a response with a message. */
 
 const logout = asyncHandler(async (req, res) => {
-  res.cookie('token', 'logout', {
-    httpOnly: true,
-    expires: new Date(Date.now()),
-  });
+    res.cookie('token', 'logout', {
+        httpOnly: true,
+        expires: new Date(Date.now()),
+    });
 
-  res.status(StatusCodes.OK).json({ message: 'Logout successfully' });
+    res.status(StatusCodes.OK).json({ message: 'Logout successfully' });
 });
 
 module.exports = { signup, login, logout, otp };
--- utils/create-otp.js
+++ utils/create-otp.js
@@ -1,46 +1,61 @@
 require('dotenv').config();
+const { StatusCodes } = require('http-status-codes');
+
 const client = require('twilio')(
-  process.env.ACCOUNT_SID,
-  process.env.AUTH_TOKEN
+   process.env.ACCOUNT_SID,
+   process.env.AUTH_TOKEN
 );
 
 const createOTP = ({ res, phonenumber, channel }) => {
-  client.verify
-    .services(process.env.SERVICE_ID)
-    .verifications.create({
-      to: `+91${phonenumber}`,
-      channel: channel === 'call' ? 'call' : 'sms',
-    })
-    .then((data) => {
-      console.log(data);
-      res.status(200).send({
-        message: 'Verification is sent!!',
-        phonenumber: phonenumber,
-        data,
+   client.verify
+      .services(process.env.SERVICE_ID)
+      .verifications.create({
+         to: `+91${phonenumber}`,
+         channel: channel === 'call' ? 'call' : 'sms',
+      })
+      .then((data) => {
+         res.status(200).send({
+            message: 'Success',
+            phonenumber: `verification send to ${phonenumber}`,
+         });
+      })
+      .catch((err) => {
+         res.status(StatusCodes.BAD_REQUEST).json({
+            message: 'something went wrong',
+            err,
+         });
       });
-    });
 };
 
 const verifyOTP = ({ res, mobileNO, code }) => {
-  console.log(mobileNO)
-  client.verify
-    .services(process.env.SERVICE_ID)
-    .verificationChecks.create({
-      to: `+91${mobileNO}`,
-      code: code,
-    })
-    .then((data) => {
-      console.log(data)
-      if (data.status === 'approved') {
-        res.status(200).send({
-          message: 'User is Verified!!',
-          data,
-        });
-      }
-    });
+   client.verify
+      .services(process.env.SERVICE_ID)
+      .verificationChecks.create({
+         to: `+91${mobileNO}`,
+         code: code,
+      })
+      .then((data) => {
+         if (data.status === 'approved') {
+            res.status(200).send({
+               message: 'User is Verified!!',
+               data,
+            });
+         } else {
+            res.status(StatusCodes.BAD_REQUEST).json({
+               message: 'OTP is not corrected',
+               phonenumber: mobileNO,
+            });
+         }
+      })
+      .catch((err) => {
+         res.status(StatusCodes.BAD_REQUEST).json({
+            message: 'Someting wrong',
+            err,
+         });
+      });
 };
 
 module.exports = {
-  createOTP,
-  verifyOTP,
+   createOTP,
+   verifyOTP,
 };
