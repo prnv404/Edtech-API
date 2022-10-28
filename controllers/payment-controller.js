cc0f1d7cc2bcc7a8a6de24b16ac5eb08b0f26956
pranav <pranavofficial404@gmail.com>
2022-10-28 17:54:34 +0530

    payment verification using web hook created

--- controllers/payment-controller.js
+++ controllers/payment-controller.js
@@ -6,10 +6,9 @@ const Course = require('../models/course-model')
 const Razorpay = require('razorpay')
 
 var instance = new Razorpay({
-   key_id: process.env.KEY_ID,
-   key_secret: process.env.KEY_SECRET,
+    key_id: process.env.KEY_ID,
+    key_secret: process.env.KEY_SECRET,
 })
-
 
 // TODO: PAYMENT VERIFICATION VIA WEBHOOKS
 /**
@@ -19,54 +18,77 @@ var instance = new Razorpay({
  */
 
 const createOrder = async (req, res) => {
-   const { price, standred } = req.body
+    const { price, standred } = req.body
 
-   if (!price || !standred) {
-      throw new CustomError.BadRequestError('Please provide all values')
-   }
+    if (!price || !standred) {
+        throw new CustomError.BadRequestError('Please provide all values')
+    }
 
-   const course = await Course.findOne({ standred })
+    const course = await Course.findOne({ standred })
 
-   if (course.pirce !== price) {
-      throw new CustomError.BadRequestError('Price dosent match')
-   }
+    if (course.pirce !== price) {
+        throw new CustomError.BadRequestError('Price dosent match')
+    }
 
-   const order = {
-      price: price,
-      course: course,
-      user: req.user.userId,
-   }
+    const order = {
+        price: price,
+        course: course,
+        user: req.user.userId,
+    }
 
-   let receiptId = Math.random() * 10000
+    let receiptId = Math.random() * 10000
 
-   receiptId.toString()
+    receiptId.toString()
 
-   const orderResult = await Order.create(order)
+    const orderResult = await Order.create(order)
 
-   price.toString()
+    price.toString()
 
-   try {
-      const order = await instance.orders.create({
-         amount: price * 100,
-         currency: 'INR',
-         receipt: receiptId,
-      })
-     
-      res.status(StatusCodes.CREATED).json({
-         success: true,
-         order,
-         price,
-      })
-   } catch (error) {
-      console.log(error)
-   }
+    try {
+        const order = await instance.orders.create({
+            amount: price * 100,
+            currency: 'INR',
+            receipt: receiptId,
+        })
+
+        res.status(StatusCodes.CREATED).json({
+            success: true,
+            order,
+            price,
+        })
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-
 const verifyPayment = async (req, res) => {
-   res.send('hello')
+    const secret = process.env.VERIFY_SECRET
+
+    console.log(req.body)
+
+    const crypto = require('crypto')
+
+    const shasum = crypto.createHmac('sha256', secret)
+    shasum.update(JSON.stringify(req.body))
+    const digest = shasum.digest('hex')
+
+    console.log(digest, req.headers['x-razorpay-signature'])
+
+    if (digest === req.headers['x-razorpay-signature']) {
+        console.log('request is legit')
+        // process it
+        require('fs').writeFileSync(
+            'payment1.json',
+            JSON.stringify(req.body, null, 4)
+        )
+    } else {
+        // pass it
+    }
+
+    res.status(StatusCodes.OK)
 }
 
 module.exports = {
-   createOrder,
+    createOrder,
+    verifyPayment,
 }
--- controllers/video-controller.js
+++ controllers/video-controller.js
@@ -82,7 +82,6 @@ const uploadVideo = async (req, res) => {
       }
    )
    
-   
 }
 
 /**
