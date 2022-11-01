efebb2558b32be9a00738220c1b2cf00bc6ba5d5
pranav <pranavofficial404@gmail.com>
2022-11-01 18:53:29 +0530

    some typo fixed

--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,5 +1,5 @@
 const notFound = async (req, res) => {
-    res.status(404).json({ message: 'Route NOt found' })
+    res.status(404).json({ message: 'Route Not found' })
 }
 
 module.exports = notFound
